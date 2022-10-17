6086e02dbe70de1d91d13c1adb6135c615ca13f7
TobiasCunnen <t.cunnen@gmail.com>
2022-10-17 11:43:55 +0200

    Redirect root page to dictionary index page

--- signapp/urls.py
+++ signapp/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
+    path('', include('dictionary.urls')),
     path('dictionary/', include('dictionary.urls')),
     path('admin/', admin.site.urls),
 ]
