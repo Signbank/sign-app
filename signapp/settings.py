31f9e1b49d36b92a25c09be480ccfbd7781086f1
TobiasCunnen <t.cunnen@gmail.com>
2022-10-17 15:02:36 +0200

    Created a base html page

--- signapp/settings.py
+++ signapp/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'signapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
