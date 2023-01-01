837ad4b6b2d1a9b9f609b5898930e447155ae7e7
TobiasCunnen <t.cunnen@gmail.com>
2023-01-01 13:38:20 +0100

    Added django rest framework

--- django/dictionary/urls.py
+++ django/dictionary/urls.py
@@ -9,6 +9,5 @@ router.register(r'signs', views.SignViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
         # path('', views.IndexView.as_view(), name='index'),
