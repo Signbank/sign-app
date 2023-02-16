15bae10c7f8e63f3a6d2a48ff5f3024d7b2c421c
TobiasCunnen <t.cunnen@gmail.com>
2023-02-16 20:36:37 +0100

    Created unit tests for the property model

---
+++ flutter/test/conntroller_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+
+  test('Create Sign object from JSON', () {
+
+  });
+}
--- flutter/test/model_test.dart
+++ flutter/test/model_test.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sign_app/models/property.dart';
 import 'package:sign_app/models/sign.dart';
 
 void main() {
-  test('Sign object should be created', () {
+  test('Create Sign object from JSON', () {
     const signName = 'test';
     const videoUrl = 'super cool link to video';
     const imageUrl = 'super cool link to image';
@@ -17,5 +18,44 @@ void main() {
     expect(sign.name, signName);
     expect(sign.videoUrl, videoUrl);
     expect(sign.imageUrl, imageUrl);
+  });
+
+  test('Create Sign list from JSON', () {
+    const signName = 'test';
+    const videoUrl = 'super cool link to video';
+    const imageUrl = 'super cool link to image';
+    const json = [
+      {'sign_name': signName, 'video_url': videoUrl, 'image_url': imageUrl}
+    ];
+
+    List<Sign> signList = Sign.listFromJson(json);
+
+    expect(signList[0].name, signName);
+    expect(signList[0].videoUrl, videoUrl);
+    expect(signList[0].imageUrl, imageUrl);
+  });
+
+  test('Create Property object from JSON', () {
+    const propertyIdentifier = 'test';
+    const signIDs = [1, 2, 3];
+    const json = {'identifier': propertyIdentifier, 'sign_ids': signIDs};
+
+    Property property = Property.fromJson(json);
+
+    expect(property.identifier, propertyIdentifier);
+    expect(property.signIDs, signIDs);
+  });
+
+  test('Create Property list from JSON', () {
+    const propertyIdentifier = 'test';
+    const signIDs = [1, 2, 3];
+    const json = [
+      {'identifier': propertyIdentifier, 'sign_ids': signIDs}
+    ];
+
+    List<Property> propertyList = Property.listFromJson(json);
+
+    expect(propertyList[0].identifier, propertyIdentifier);
+    expect(propertyList[0].signIDs, signIDs);
   });
 }
